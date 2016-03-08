7e220527a89742663ad4282ebed4d27f1bae3b8c
Scott Walter <swalter@cray.com>
2016-03-08 11:41:08 -0600

    differentiated name of returned object from class

--- src/main/java/com/cray/stash/Message.java
+++ src/main/java/com/cray/stash/Message.java
@@ -5,17 +5,15 @@ import java.util.HashMap;
 
 public class Message {
 
-    private HashMap<String, Object> message;
+    private HashMap<String, Object> content;
     private String topic;
 
-    public Message(HashMap<String, Object> message, String topic) {
+    public Message(HashMap<String, Object> content, String topic) {
         this.topic = topic;
-        this.message = message;
+        this.content = content;
     }
 
-    public HashMap<String, Object> getMessage() {
-        return message;
-    }
+    public HashMap<String, Object> getMessage() {return content;}
 
     public String getTopic() {return topic;}
 }
