da17aa538bd5de0f1422f98e815fa1778792e335
Scott Walter <swalter@cray.com>
2016-06-07 14:52:02 -0500

    Putting all the fedmsg stuff in the same place.
    I want all the methods that deal with the message class to be in the message class
    hence me adding the method that actually sends the message in this class.

--- src/main/java/com/cray/stash/Message.java
+++ src/main/java/com/cray/stash/Message.java
@@ -1,12 +1,18 @@
 package com.cray.stash;
 
+import org.fedoraproject.fedmsg.FedmsgConnection;
+import org.fedoraproject.fedmsg.FedmsgMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class Message {
 
     private HashMap<String, Object> content;
     private String topic;
+    private final static Logger log = LoggerFactory.getLogger(Message.class);
 
     public Message(HashMap<String, Object> content, String topic) {
         this.topic = topic;
@@ -16,4 +22,24 @@ public class Message {
     public HashMap<String, Object> getMessage() {return content;}
 
     public String getTopic() {return topic;}
+
+    /*
+     * This is a helper method for all events, it simply sends the message to Fedmsg with
+     * a specified topic and prepends an topic prefix, environment, and modname.
+     */
+    public void sendMessage(FedmsgConnection connection) {
+        log.info("Sending fedmsg message...");
+        FedmsgMessage msg = new FedmsgMessage(
+                content,
+                topic.toLowerCase(),
+                (new java.util.Date()).getTime() / 1000,
+                1);
+        try {
+            connection.send(msg);
+        } catch (IOException e) {
+            log.error("IOException occurred when sending fedmsg message: " + e.getMessage());
+        } catch (Exception e) {
+            log.error("Exception occurred when sending fedmsg message: " + e.getMessage());
+        }
+    }
 }
