ea77c357044679bf18917669859e7f4fbc9b55ce
Scott Walter <swalter@cray.com>
2016-07-20 12:20:00 -0500

    Logging  dependency, version update, JRE version update

---
+++ src/main/java/com/cray/stash/EventLoggerFactory.java
@@ -0,0 +1,90 @@
+package com.cray.stash;
+
+import java.io.File;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
+import com.atlassian.sal.api.ApplicationProperties;
+
+/**
+ * Created by swalter on 7/8/2016.
+ */
+public class EventLoggerFactory {
+
+    private static final String ROOT = "com.palantir.stash.signupmanager";
+    private static final Logger stashRootLogger = LoggerFactory.getLogger("ROOT");
+
+    private LoggerContext context;
+
+    private final String homeDir;
+
+    public EventLoggerFactory(ApplicationProperties applicationProperties) {
+        homeDir = applicationProperties.getHomeDirectory().getAbsolutePath();
+        init();
+    }
+
+    public EventLoggerFactory() {
+        homeDir = new File(".").getAbsolutePath();
+        init();
+    }
+
+    private void init() {
+        // Assumes LSF4J is bound to logback
+        context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        // store the home dir to use for relative paths
+        context.putProperty("stash.home", homeDir);
+
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(context);
+
+        InputStream is;
+        is = this.getClass().getClassLoader().getResourceAsStream("logback-test.xml");
+        if (is != null) {
+            stashRootLogger.info("Using logback-test.xml for logger settings");
+        } else {
+            stashRootLogger.info("Using logback.xml for logger settings");
+            is = this.getClass().getClassLoader().getResourceAsStream("logback.xml");
+        }
+
+        try {
+            configurator.doConfigure(is);
+        } catch (JoranException e) {
+            System.err.println("Error configuring logging framework" + e);
+        }
+    }
+
+    public Logger getLogger() {
+        return getLogger(ROOT);
+    }
+
+    public Logger getLogger(String name) {
+        return context.getLogger(name);
+    }
+
+    public Logger getLogger(Class<? extends Object> clazz) {
+        String className = clazz.toString();
+        if (className.startsWith("class ")) {
+            className = className.replaceFirst("class ", "");
+        }
+
+        return context.getLogger(className);
+
+    }
+
+    public Logger getLoggerForThis(Object obj) {
+        String className = obj.getClass().toString();
+        if (className.startsWith("class ")) {
+            className = className.replaceFirst("class ", "");
+        }
+
+        return context.getLogger(className);
+
+    }
+}
